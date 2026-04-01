bb7fcdbcddb11b44052303008ba3a13bf0cf061f
Khuswant Rajpurohit <khuswantrajpurohit18@gmail.com>
2026-04-02 02:19:18 +0530

    feat: add fsevents package for MacOS file system events support
    feat: create initial index component for frontend application

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import 'dotenv/config'
 
 const app = express()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 4000
 
 app.get('/',(req,res)=>{
     res.send("Server is healthy")
 })
 
-app.listen(()=>{
+app.listen(PORT,()=>{
     console.log("server is running on port",PORT)
 })
